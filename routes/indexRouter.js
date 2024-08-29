3266339c04b16ec15526e365a1342f197cd3267c
SDMckone <seandmckone@gmail.com>
2024-08-29 04:13:13 -0400

    Implement message view

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,34 +1,55 @@
 const { Router } = require('express');
 const indexRouter = Router();
 
+
 const messages = [
   {
+    id: 0,
     text: "Hi there!",
     user: "Gadget",
     added: new Date()
   },
   {
+    id: 1,
     text: "Hello World!",
-    user: "Wildfyre",
+    user: "WildFire",
     added: new Date()
   }
 ];
 
+let messageId = messages.length;
+
+
 indexRouter.get('/', (req, res) => {
   res.render('index', { messages: messages });
+});
+
+indexRouter.get('/messages/:id', (req, res) => {
+  if (messages[req.params.id]) { 
+    res.render('message', { message: messages[req.params.id] });
+  }
+  else {
+    res.render('404');
+  }
 });
 
 indexRouter.get('/new', (req, res) => {
   res.render('form');
 });
 
+indexRouter.get('*', (req, res) => {
+  res.render('404');
+});
+
 indexRouter.post('/new', (req, res) => {
-  messages.unshift({ 
+  messages.push({ 
+    id: messageId,
     text: req.body.messageBody, 
     user: req.body.username, 
     added: new Date() });
 
-  res.redirect("/")
+  res.redirect("/");
+  messageId++;
 });
 
 module.exports = indexRouter; 
