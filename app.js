58ee5d7a893370c39d6cd43aaf77b473b57602df
SDMckone <seandmckone@gmail.com>
2024-08-29 05:25:14 -0400

    Add initial styling

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const path = require("node:path");
+const assetsPath = path.join(__dirname, "public");
+
 const indexRouter = require('./routes/indexRouter.js');
 
 const express = require('express');
@@ -10,6 +12,7 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(assetsPath));
 
 app.use('/', indexRouter);
 
